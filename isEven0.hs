5b352f854b8155467d5b7d57a9a708499d94e7e4
Jordi Ricarte <jpricarte@inf.ufrgs.br>
2020-08-05 01:21:43 -0300

    fixing bugs

--- isEven0.hs
+++ isEven0.hs
@@ -1,11 +1,16 @@
 -- Created by Jordi Ricarte
-isEvenAux :: (Ord a) => a -> [a] -> Bool
+isEvenAux :: (Ord a, Num a) => a -> [a] -> Bool
 isEvenAux n listEven
     | n == thisNum = True
-    | n > thisNum = False
+    | n == thisNumPlusOne = False
     | otherwise = isEvenAux n (tail listEven)
     where
         thisNum = head listEven
+        thisNumPlusOne = 1 + thisNum
 
 isEven :: (Ord a, Num a, Enum a) => a -> Bool
-isEven n = isEvenAux n [2,4 .. ]
+isEven n = if n>=0 
+    then isEvenAux n l
+    else isEvenAux (abs n) l
+    where
+        l = [0,2 .. ]
